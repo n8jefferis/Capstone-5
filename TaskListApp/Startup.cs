a400f3664f504355298ec7c93524a9b8c4bdaf14
Your Name <n8jefferis@gmail.com>
2019-05-27 13:48:56 -0400

    Add project files.

---
+++ TaskListApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TaskListApp.Startup))]
+namespace TaskListApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
